ee296734d6ba8065646e3052567165cbe0d39538
Cornellius Ngondo <devngondo@gmail.com>
2015-04-24 17:29:06 +0000

    Solved an error in showing the root/index file. Deprecated method 'attr_accessible'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(new_user)
+
+    if @user.save
+      redirect_to @user, notice: "Thank you for signing up for Ribbit!"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def new_user
+    params.require(:user).permit(:avatar_url, :email, :name, :password, :password_confirmation, :username)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
   before_save :create_avatar_url
-  attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
+  #Deprecated in rails 4.x.x and above
+  #attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
   #The gem bcrypt works with the following line of code to encrypt the password
   has_secure_password
   before_validation :prep_email
   #These are the validations to contraint the user
   validates :name, presence: true
   validates :username, uniqueness: true, presence: true
-  validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
+  validates :email, uniqueness: true, presence: true, format: { with: /\A^[\w.+-]+@([\w]+.)+\w+$\z/ }
 
   #Method that takes up the email to match with the avatar image
   private
@@ -18,6 +19,6 @@ class User < ActiveRecord::Base
     def create_avatar_url
       self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
     end
-  #end
+
 
 end
