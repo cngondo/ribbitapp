0db9951fe5bfa2bb7d64dbad3d196945fda7b932
Cornellius Ngondo <devngondo@gmail.com>
2015-04-26 22:54:12 +0000

    Ribbit functionality created

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,6 @@ class User < ActiveRecord::Base
     def create_avatar_url
       self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
     end
-
-
+  #A user has many tweets
+  has_many :ribbits
 end
